ac1fef6f108867af154acb891b470aefe0e1717a
JimCampagno <jimcampagno@gmail.com>
2016-06-07 14:09:37 -0400

    setup playground file

--- DictionaryIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ DictionaryIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -1,17 +1,18 @@
 /*: Outline
  
  
- # Arrays & array iteration
+ # Iterating over a Dictionary
  
  ### Readings associated with this lab
  
- * [Array](https://github.com/learn-co-curriculum/swift-array-readme)
- * [Array Iteration](https://github.com/learn-co-curriculum/swift-arrayIteration-readme)
+ * [Dictionary](https://github.com/learn-co-curriculum/swift-dictionary-readme)
+ * [Iterating over a Dictionary](https://github.com/learn-co-curriculum/swift-dictionaryIteration-readme)
  
 
  */
 /*: question1
- ### 1. You're building an app and in a particular ViewController you want to store all of the ingredients people are adding to a cart. How best could we represent this cart in code? The ingredients are "Chips", "Salsa", "Guacamole", "Red wine". Create a variable that represents our cart which stores these ingredients.
+ ### 1. 
+ 
  */
 // write your code here
 
@@ -36,9 +37,9 @@
 
 
 /*: question4
- ### 4. Create a function that takes an array of names and greets each person with the following message "Good morning x" <- x being the individuals name. One caveat. If the persons first name is Michael, we want to greet them with the message, "Top of the morning Michael!".
+ ### 4. Q
  */
-let people = ["Maryann", "Francis", "Joe", "Michael"]
+
 
 
 
@@ -49,11 +50,9 @@ let people = ["Maryann", "Francis", "Joe", "Michael"]
 
 
 /*: question5
- ### 5. Create a function that takes an array of Ints and returns an array of Strings. This function should look through the array of Ints its getting and create a new array of Ints that only contain integers less than 50.
+ ### 5. QQ
  */
-let grades = [40, 60, 50, 52, 59, 13, 90, 100, 5, 52, 51, 49]
-// if we passed grades into your function, it should return the following
-// [40, 13, 5, 49]
+
 
 
 
