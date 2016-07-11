a86adee97a217c5e6bae64dae065ce50eb71578e
zainnadeem <zn.nadeem@gmail.com>
2016-07-11 13:23:37 -0400

    iteration

--- DictionaryIteration.playground/Contents.swift
+++ DictionaryIteration.playground/Contents.swift
@@ -19,20 +19,24 @@
 // write your code here
 
 
-
+var bandAndHighestSellingAlbum = ["Nirvana": "Nevermind",
+    "Blondie" : "Parallel Lines",
+    "The Kinks" : "Low Budget",
+    "The Beatles" : "Sgt. Pepper's Lonely Hearts Club Band"]
 
 /*: question2
  ### 2. Oops, we forgot an album! Add the Pixie's _Doolittle_ to the dictionary you created in Question 1.
  */
 // write your code here
 
-
+bandAndHighestSellingAlbum["Pixie's"] = "Doolittle"
 
 
 /*: question3
  ### 3. We don't care that much about The Kinks. Delete them from the dictionary you created in Question 1.
  */
 // write your code here
+        bandAndHighestSellingAlbum["The Kinks"] = nil
 
 
 
@@ -44,6 +48,7 @@
 let bandName = "Nirvana"
 // write your code here
 
+print ("\(bandName) top-selling album was \(bandAndHighestSellingAlbum["\(bandName)"]!)")
 
 
 
@@ -53,19 +58,21 @@ let bandName = "Nirvana"
 // write your code here
 
 
-
+for (band, album) in bandAndHighestSellingAlbum{
+    print("\(band) top-selling album was \(album)")
+}
 
 /*: question6
  ### 6. What type of data structure is returned when you iterate over a dictionary?
  */
-
+//array
 
 
 
 /*: question7
  ### 7. Imagine every student in every grade in a high school took an exam. Each student got a letter grade based on their test results (A, B, C, D, and F). The results were averaged together per grade. How could you represent this mapping of grades to exam average?
  */
-
+// dictionary [9:average, 10:average, 11:average, 12:average]
 
 
 
@@ -80,23 +87,28 @@ let bandName = "Nirvana"
 // write your code here
 
 
-
+let resultsPerGrade = [9:"C", 10:"C", 11:"B", 12:"C", 13:"C"]
 
 /*: question9
  ### 9. Print the message "Grade <Grade> got <Score>" for each grade in the dictionary you created in Question 8.
  */
 // write your code here
 
-
-
+var arrayOfResults:[String] = []
+for (grade, score) in resultsPerGrade{
+    arrayOfResults.append(score)
+}
 
 /*: question10
  ### 10. Find which grade got the highest score! Iterate through the dictionary to find the highest score, then print the message "Grade <Grade> got the highest score with <Score>!" to the console.
  */
 // write your code here
 
-
-
+for (grade, result) in resultsPerGrade{
+    if result == arrayOfResults.sort().first!{
+    "Grade \(grade) got \(result)"
+}
+}
 
 /*:
  [Solution](solution)
